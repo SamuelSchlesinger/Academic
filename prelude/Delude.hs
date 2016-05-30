15b145991b1ddf468e39fa5a15c2ad9ee23b862b
Samuel Schlesinger <sgschlesinger@gmail.com>
2016-05-30 16:32:27 -0400

    delude edits

--- prelude/Delude.hs
+++ prelude/Delude.hs
@@ -1,3 +1,43 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, NoImplicitPrelude, ScopedTypeVariables #-}
+
+
+module Delude where
+
+import Prelude hiding ((||), (&&), (^), iff, implies, not)
+import qualified Data.IntMap as IntMap
+
+-- | A more general logical interface named after the
+-- | great man George Boole
+class GeorgeLike b where
+    (||)     :: b -> b -> b
+    (&&)     :: b -> b -> b
+    (^)      :: b -> b -> b
+    iff      :: b -> b -> b
+    implies  :: b -> b -> b
+    not      :: b -> b
+
+instance GeorgeLike Bool where
+    False || False = False
+    _     || _     = True
+    True  && True  = True
+    _     && _     = False
+    False ^  True  = True
+    True  ^ False  = True
+    _     ^ _      = False
+    True  `iff` True = True
+    False `iff` False = True
+    _     `iff` _     = False
+    True `implies` False = False
+    _ `implies` _ = True
+    not True = False
+    not False = True
+
+instance (GeorgeLike b) => GeorgeLike (x -> b) where
+    f || g = \x -> (f x) || (g x)
+    f && g = \x -> (f x) && (g x)
+    f ^  g = \x -> (f x) ^  (g x)
+    f `iff` g = \x -> (f x) `iff` (g x)
+    f `implies` g = \x -> (f x) `implies` (g x)
+    not f = \x -> not (f x)
 
 
